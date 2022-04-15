1a6da27928cca85b767e53fa24e0d44101faee85
Samuel Jacobs <sammyjacobscontact@gmail.com>
2022-04-15 13:00:03 +0100

    updated .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
   return {
     dir: {
       input: "src",
